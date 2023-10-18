b6f1b0e204dd99c7090f666ffe6d23ae98e7e371
Swathi <ssspriyamoru@gmail.com>
2023-10-18 15:02:50 +0530

    new commit

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -5,8 +5,6 @@ import { DateTime } from "luxon";
 const API_KEY = "50c0ca57c0208364f266c848e9b59dd8";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
-
-
 // https://api.openweathermap.org/data/2.5/onecall?lat=28.6128&lon=77.2311&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
 
 const getWeatherData = (infoType, searchParams) => {
@@ -19,7 +17,7 @@ const getWeatherData = (infoType, searchParams) => {
 const formatCurrentWeather = (data) => {
   const {
     coord: { lat, lon },
-   
+
     main: { temp, feels_like, temp_min, temp_max, humidity },
     name,
     dt,
@@ -31,9 +29,7 @@ const formatCurrentWeather = (data) => {
 
   const { main: details, icon } = weather[0];
   // console.log(timezone);
- 
-  
- 
+
   return {
     lat,
     lon,
@@ -50,63 +46,31 @@ const formatCurrentWeather = (data) => {
     details,
     icon,
     speed,
-    timezone
+    timezone,
   };
-   
- 
 };
 
-
-
-  
-
-  // return {daily};
+// return {daily};
 
 // formatForecastWeather();
 const getFormattedWeatherData = async (searchParams) => {
- 
-
   const formattedCurrentWeather = await getWeatherData(
     "weather",
     searchParams
   ).then(formatCurrentWeather);
 
- 
-
-
-  
-
-  
-
-
-  
-
-  
-
-
   return { ...formattedCurrentWeather };
-
-  
 };
 
 const formatToLocalTime = (
-	secs,
-	zone,
-	format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+  secs,
+  zone,
+  format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
 ) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
-
-
 
 const iconUrlFromCode = (code) =>
   `http://openweathermap.org/img/wn/${code}@2x.png`;
 
+export default getFormattedWeatherData;
 
-
-
-
-  export default getFormattedWeatherData;
-
-export { formatToLocalTime,iconUrlFromCode };
-
-
-
+export { formatToLocalTime, iconUrlFromCode };
